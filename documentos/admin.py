1721fe1d5249152dc31c6bcb6fcb50409840d325
Presley Mcquade Nogueira <presley_mc@hotmail.com>
2013-06-18 09:59:18 -0300

    FILTRANDO/PERSONALIZANDO OS REGISTROS EXIBIDOS EM UM FIELD FOREIGNKEY POR USUÁRIO LOGADO

--- documentos/admin.py
+++ documentos/admin.py
@@ -7,6 +7,7 @@ class DocumentoAdmin(admin.ModelAdmin):
     list_filter = (FilterTipoDocumento,)
     list_display = ('nome', 'numero','ano','tipo', 'user')
     exclude = ['user',]
+
     def save_model(self, request, obj, form, change):
         if not obj.user:
             obj.user = request.user
@@ -17,6 +18,14 @@ class DocumentoAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return qs
         return qs.filter(user=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        user = request.user
+
+        if db_field.name == "tipo":
+            kwargs["queryset"] = TipoDocumento.objects.filter(groups__in = user.groups.all())
+            return db_field.formfield(**kwargs)
+        return super(DocumentoAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 class TipoDocumentoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'slug',)
